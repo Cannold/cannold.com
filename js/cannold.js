a3ec84723ab5fca89c889ca8f93c96f3292fc48b
Liam Cannold <Liamcannold@gmail.com>
2019-09-05 17:12:43 +1000

    created an events type like the blog tab

--- js/cannold.js
+++ js/cannold.js
@@ -27,6 +27,7 @@ function updateSplash() {
   });
 }
 
+// Blog splash function //
 function updateBlogPreview() {
 
 
@@ -57,6 +58,9 @@ function updateBlogPreview() {
    // id is ghost-latest-posts
 
 
+
+
+
     // this is where the page update happens
 
   })
@@ -66,9 +70,96 @@ function updateBlogPreview() {
 
 }
 
+function updateBlogPreview() {
+
+
+  $('#ghost-latest-posts').html(''); // we want it blanked out
+
+  var latestPostsHTML = "";
+
+  ghostAPI.posts.browse({
+    limit: '4',
+    filter: 'tags:-[splash,Events]',
+  })
+  .then((posts) => {
+
+    posts.forEach((post) => {
+
+      // use jQuery to dyamically add the post elements
+
+      latestPostsHTML = '<a href="' + post.url + '">';
+      latestPostsHTML += '<div>';
+      latestPostsHTML += '<h3>' + post.title + '</h3>';
+      latestPostsHTML += '<p>' + post.excerpt + '</p>';
+      latestPostsHTML += '</div></a>';
+
+      $('#ghost-latest-posts').append(latestPostsHTML);
+
+    });
+
+   // id is ghost-latest-posts
+
+
+
+
+
+    // this is where the page update happens
+
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+}
+
+function updateEventPreview() {
+
+
+  $('#ghost-latest-events').html(''); // we want it blanked out
+
+  var latestPostsHTML = "";
+
+  ghostAPI.posts.browse({
+    limit: '4',
+    filter: 'tags:[Events]',
+    
+  })
+  .then((posts) => {
+
+    posts.forEach((post) => {
+
+      // use jQuery to dyamically add the post elements
+
+      latestEventsHTML = '<a href="' + post.url + '">';
+      latestEventsHTML += '<div>';
+      latestEventsHTML += '<h3>' + post.title + '</h3>';
+      latestEventsHTML += '<p>' + post.excerpt + '</p>';
+      latestEventsHTML += '</div></a>';
+
+      $('#ghost-latest-events').append(latestEventsHTML);
+
+    });
+
+   // id is ghost-latest-posts
+
+
+
+
+
+    // this is where the page update happens
+
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+}
+
+
 function updateGhostContent() {
   updateSplash();
   updateBlogPreview();
+  updateEventPreview();
 }
 
 $(document).ready( updateGhostContent() );
